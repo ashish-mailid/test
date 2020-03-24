e58e3e28bff808a62173327a16dc628573a67438
Ashish Shah <62329081+ashish-mailid@users.noreply.github.com>
2020-03-24 14:25:19 +0530

    Create JaxbExample.java

---
+++ src/main/com/thebuzzmedia/sjxp/rule/JaxbExample.java
@@ -0,0 +1,46 @@
+package com.howtodoinjava.demo;
+ 
+import java.io.File;
+import java.io.StringWriter;
+ 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+ 
+public class JaxbExample 
+{
+    public static void main(String[] args) 
+    {
+        //Java object. We will convert it to XML.
+        Employee employee = new Employee(1, "Lokesh", "Gupta", new Department(101, "IT"));
+         
+        //Method which uses JAXB to convert object to XML
+        jaxbObjectToXML(employee);
+    }
+ 
+    private static void jaxbObjectToXML(Employee employee) 
+    {
+        try
+        {
+            //Create JAXB Context
+            JAXBContext jaxbContext = JAXBContext.newInstance(Employee.class);
+             
+            //Create Marshaller
+            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+ 
+            //Required formatting??
+            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+ 
+           //Store XML to File
+            File file = new File("employee.xml");
+             
+            //Writes XML file to file-system
+            jaxbMarshaller.marshal(employee, file); 
+        } 
+        catch (JAXBException e) 
+        {
+            e.printStackTrace();
+        }
+    }
+}
+
